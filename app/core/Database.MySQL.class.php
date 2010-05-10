a29094b139f033122819df3e63a30f0bd770af89
Pavel Vyazovoi <vyazovoi@googlemail.com>
2010-05-10 12:57:07 +0600

    Кое-что по БД

--- app/core/Database.MySQL.class.php
+++ app/core/Database.MySQL.class.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * MySQL
- * 
+ *
  * Класс реализует работу с БД MySQL
  * Дополнительно описание см. Database
- * 
+ *
  * Собственные методы:
  * 1) escape - экранирует переданный параметр
  * 2) insert_id - mysql_insert_id
@@ -12,36 +12,35 @@
  *
  * @version 0.1
  * @package Core
- * @todo fetchRow, fetchCol
  */
 class MySQL extends Database {
-    
+
     /**
      * Конструктор: инициализация подключения к базе данных
      */
-    function __construct() {
-        $this->descriptor=mysql_pconnect($db_host,$db_login,$db_password) or die(mysql_error()); // FIXME: заменить or die на вменяемый обработчик экзепшенов
-        mysql_select_db($db_name,$this->descriptor);
+    function connect() {
+        $this->descriptor=mysql_pconnect($dbHost,$this->core->config->database->dbUser,$this->core->config->database->dbPassword) or die(mysql_error()); // FIXME: заменить or die на вменяемый обработчик экзепшенов
+        mysql_select_db($this->core->config->database->dbName,$this->descriptor);
         $this->exec("SET NAMES 'UTF8'");
         return 0;
     }
-    
+
     /**
      * Exec
-     * 
+     *
      * Функция выполняет запрос без обработки результата
-     * 
+     *
      * @param string $query SQL-запрос
      */
     function exec($query) {
         mysql_query($query,$this->descriptor);
     }
-    
+
     /**
      * Escape
-     * 
+     *
      * Функция корректно экранирует текст
-     * 
+     *
      * @param string $string Строка, которую необходимо экранировать
      * @return string Экранированная строка
      */
@@ -56,12 +55,12 @@ class MySQL extends Database {
      *
      * @param string $query SQL-запрос
      * @return array массив с результатом запроса
-     */    
+     */
     function fetchAll($query) {
         $result=mysql_unbuffered_query($query,$this->descriptor);
         return @mysql_fetch_array($result);
     }
-    
+
     /**
      * fetchRow
      *
@@ -73,6 +72,16 @@ class MySQL extends Database {
     function fetchRow($query) {
         $result=mysql_unbuffered_query($query,$this->descriptor);
         return @mysql_fetch_array($result);
+    }
+
+    // TODO: реализовать
+    function fetchCol() {
+        return 1;
+    }
+
+    // TODO: реализовать
+    function fetchOne() {
+        return 1;
     }
 
 }
--- app/core/Database.class.php
+++ app/core/Database.class.php
@@ -1,35 +1,45 @@
 <?php
 /**
  * Database
- * 
+ *
  * Абстрактный класс для работы с базами данных
- * 
+ *
  * Абстрактные методы:
  * 1) exec     - выполняет запрос без обработки результата
  * 2) fetchAll - возвращает все записи по запросу
  * 3) fetchRow - возвращает одну строку
  * 4) fetchCol - возвращает столбец
  * 5) fetchOne - возвращает скалярное значение
- * 
+ *
  * @version 0.1
  * @package Core
  */
 abstract class Database {
-	
-	public $db_host;
-	public $db_name;
-	public $db_login;
-	public $db_password;
-	
+
+	public $dbHost;
+	public $dbName;
+	public $dbUser;
+	public $dbPassword;
+
+	protected $core;
+
+	function Database($core) {
+        $this->core = $core;
+        $this->dbHost = $this->core->config->dbHost;
+        $this->dbName = $this->core->config->dbName;
+        $this->dbUser = $this->core->config->dbUser;
+        $this->dbPassword = $this->core->config->dbPassword;
+    }
+
 	/**
 	 * Дескриптор подключения к БД
 	 */
 	protected $descriptor;
-	
-	abstract function fetchAll($query);
+
+    abstract function fetchAll($query);
 	abstract function fetchRow($query);
 	abstract function fetchCol($query);
 	abstract function fetchOne($query);
-	
+
 }
 ?>
