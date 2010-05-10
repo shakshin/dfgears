9d4b54ab67f255a47e82840490c4a3c6f4a3c5be
Pavel Vyazovoi <vyazovoi@googlemail.com>
2010-05-10 12:16:37 +0600

    Fixed typo

--- app/modules/Test.module.php
+++ app/modules/Test.module.php
@@ -3,7 +3,7 @@ class test extends DFModule {
 
     public function main() {
         $tpl = new DFTemplater();
-        $tpl->assign("megatest", $this->core->config->database->className);
+        $tpl->assign("megatest", $this->core->config->database->system);
         return $tpl->fetch("Test");
     }
 }
