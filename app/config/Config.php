5bf86536bc71348a9e06f7e336082245b0c7d2ed
Pavel Vyazovoi <vyazovoi@googlemail.com>
2010-05-10 12:16:01 +0600

    Изменил имя параметра на более понятное

--- app/config/Config.php
+++ app/config/Config.php
@@ -1,5 +1,5 @@
 <?php
-    $cfg->database->className = "MySQL";
+    $cfg->database->system = "MySQL";
     $cfg->database->dbHost = "localhost";
     $cfg->database->dbName = "dfgears";
     $cfg->database->dbUser = "diffy";
