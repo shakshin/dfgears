cb9438d8800216595524995392f5036b18aa009a
Сергей Шакшин <rigid.mgn@gmail.com>
2010-09-23 13:59:04 +0600

    правки

--- app/templates/main.template.php
+++ app/templates/main.template.php
@@ -6,6 +6,22 @@
 		<title><?=$this->vars["pageTitle"] ?></title>
 	</head>
 	<body>
+            <div align="right">
+                <a href="/">/</a>
+                <?
+                if ($this->core->auth->check()) {
+                ?>
+                    <a href="/user/profile"><?=$_SESSION["userName"] ?></a>
+                    <a href="/user/logoff">Выход</a>
+                <?
+                } else {
+                ?>
+                    <a href="/user/logon">Вход</a>
+                <?    
+                }
+                ?>
+            </div>
+            <hr/>
             <?=$content ?>
             <hr/>
             <address>dfgears engine v<?=$this->version ?> &copy; <a href="http://diffy.ru">Different</a>; original code by <a href="mailto:shakshin@diffy.ru">Sergei Shakshin</a></address>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
+    ob_start();
     require_once "app/core/Core.class.php";
     $application = new DFCore();
     echo $application->run();
+    ob_end_flush();
 ?>  
