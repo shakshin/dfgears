7d6380adc7bc22ed1c94e3c7563657d0f764735f
Sergei Shakshin <rigid.mgn@gmail.com>
2010-10-16 00:28:57 +0600

    Админка с дизайном

--- app/templates/admin.template.php
+++ app/templates/admin.template.php
@@ -3,56 +3,90 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-                <link rel="stylesheet" type="text/css" href="/css/admin.css" media="screen"/>
-                <link rel="stylesheet" type="text/css" href="/css/superfish.css" media="screen"/>
-                <link rel="stylesheet" type="text/css" href="/css/superfish-navbar.css" media="screen"/>
-                <script type="text/javascript" src="/js/jquery-1.2.6.min.js"></script>
-                <script type="text/javascript" src="/js/hoverIntent.js"></script>
-                <script type="text/javascript" src="/js/superfish.js"></script>
-                
+		<title>DFGears Administrator</title>
+		<link href="/css/admin-main.css" rel="stylesheet" type="text/css" />
+		
+		<script type="text/javascript" charset="utf-8" src="http://code.jquery.com/jquery-1.4.1.min.js"></script>
+		<script type="text/javascript" charset="utf-8">
+			$(function() {
+				$('#menu li').hover(function() {
+					$(this).addClass('b-opened');
+				}, function() {
+					$(this).removeClass('b-opened');
+				});
 
-		<title>DFGears administrator console</title>
+				var $buttons_list = $('.b-button.b-list')
+
+				$(document).eq(0).click(function() {
+					$buttons_list.removeClass('b-opened');
+				});
+
+				$buttons_list.click(function() {
+					var $self = $(this),
+						opened = $self.hasClass('b-opened');
+
+					$buttons_list.removeClass('b-opened');
+
+					if (!opened) $self.addClass('b-opened');
+					
+					return false;	
+				});
+
+				var search_value = 'поиск';
+				$('#search_input').focus(function() {
+					var $self = $(this);
+					if ($self.val() == search_value) $self.val('');
+				});
+				$('#search_input').blur(function() {
+					var $self = $(this);
+					if ($self.val() == '') $self.val(search_value);
+				});
+			});
+		</script>
 	</head>
 	<body>
-            <div>
-            <ul id="menu" class="sf-menu sf-navbar">
-<?
-    foreach ($adminMenu as $menuItem) {
-?>
-                <li><a href="#"><?=$menuItem["title"]?></a>
-                    <?
-                    if (!empty($menuItem["menu"])) {
-                    ?>
-                    <ul>
-                    <?
-                        $alias = $menuItem["alias"];
-                        foreach ($menuItem["menu"] as $key => $value) {
-                    ?>
-                        <li><a href="/admin/<?=$alias ?>/<?=$value ?>"><?=$key ?></a></li>
-                    <?
-                        }
-                    ?>
-                    </ul>
-                    <?
-                    }
-                    ?>
-                </li>
-<?
-    }
-?>
-            </ul>
-            </div>
-             <script type="text/javascript">
 
-                // initialise plugins
-                jQuery(function(){
-                        jQuery('ul.sf-menu').superfish();
-                });
+		<div class="l-header">
+                        
+			<ul class="b-menu" id="menu">
+                                <? foreach ($adminMenu as $modMenu) { $alias = $modMenu["alias"];
+                                ?>
+				<li class="l">
+					<a href="#">
+						<span><?=$modMenu["title"] ?></span>
+					</a>
 
-                </script>
-            <div id="container">
-            <div id="content">
-            <?=$content ?>
-            </div></div>
-        </body>
+					<div class="b-submenu">
+                                                <? foreach ($modMenu["menu"] as $title => $action) { ?>
+						<a href="/admin/<?=$alias ?>/<?=$action ?>">
+							<span>
+								<?=$title ?>
+							</span>
+						</a>
+                                                <? } ?>
+					</div>
+				</li>
+                                <? } ?>
+			</ul>
+
+			<div class="b-admin-buttons r">
+				<a href="#" class="b-admin l"><?=$_SESSION["userName"] ?></a>
+				<a href="/user/logoff" class="b-logout l">Выход</a>
+			</div>
+
+			<div class="c"></div>
+
+		</div>
+
+		<div class="l-content-handler">
+
+			<div class="l-content">
+
+			<?=$content ?>
+
+			</div>
+
+		</div>
+
+	</body>
 </html>
--- app/templates/users/userlist.template.php
+++ app/templates/users/userlist.template.php
@@ -1,24 +1,80 @@
-<h1>Список пользователей</h1>
-<table class="data">
-    <tr>
-        <th>id</th>
-        <th>login</th>
-        <th>Полное имя/ник</th>
-        <th>E-mail</th>
-    </tr>
+<h2>
+					Список пользователей
+</h2>
 
-<? foreach ($users as $user) {?>
-    <tr>
-        <td><?=$user["id"] ?></td>
-        <td><?=$user["login"] ?></td>
-        <td><?=$user["fullName"] ?></td>
-        <td><a href="mailto:<?=$user["email"]?>"><?=$user["email"] ?></a></td>
-        <td>
-            [&nbsp;<a href="/admin/users/deleteUser/id/<?=$user["id"] ?>">Удалить</a>&nbsp;]
-            <? if ($user["active"] == 0) { ?>
-            [&nbsp;<a href="/admin/users/activateUser/id/<?=$user["id"] ?>">Активировать</a>&nbsp;]
-            <? } ?>
-        </td>
-    </tr>
+
+
+<div class="c"></div>
+
+<div class="b-obj-list">
+
+    <table cellspacing="0">
+        <thead>
+            <tr>
+                <th>
+										id
+                </th>
+                <th>
+										login
+                </th>
+                <th>
+										Полное имя/ник
+                </th>
+                <th>
+										Email
+                </th>
+                <th>
+                    Действия
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <? foreach ($users as $user) {
+ ?>
+                <tr>
+                    <td>
+<?= $user["id"] ?>
+                </td>
+                <td>
+<?= $user["login"] ?>
+                </td>
+                <td>
+<?= $user["fullName"] ?>
+                </td>
+                <td>
+                    <a href="mailto:<?= $user["email"] ?>"><?= $user["email"] ?></a>
+                </td>
+                <td>
+
+                    <div class="b-actions l">
+                        <div href="" class="b-button l">
+                            <a href="/admin/users/deleteUser/id/<?=$user["id"] ?>">
+                                <span>Удалить</span>
+                            </a>
+                        </div>
+
+                        <hr class="l" />
+
+                        <div class="c"></div>
+                    </div>
+
+                    <div class="b-actions l">
+                        <div href="" class="b-button l">
+                            <a href="/admin/users/activateUser/id/<?=$user["id"] ?>">
+                                <span>Активировать</span>
+                            </a>
+                        </div>
+
+                        <hr class="l" />
+
+                        <div class="c"></div>
+                    </div>
+                </td>
+            </tr>
 <? } ?>
-</table>
+        </tbody>
+    </table>
+
+  
+
+</div>
