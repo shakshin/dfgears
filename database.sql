bacddeb7ff1459edd32dbe7488b17dc3e1058498
Sergei Shakshin <rigid.mgn@gmail.com>
2010-10-16 14:15:16 +0600

    дамп базы с дефолтным админом

--- database.sql
+++ database.sql
@@ -1,5 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb1
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Окт 16 2010 г., 14:14
+-- Версия сервера: 5.1.49
+-- Версия PHP: 5.3.2-2
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `dfgears`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Структура таблицы `acl`
@@ -11,6 +31,13 @@ CREATE TABLE IF NOT EXISTS `acl` (
   KEY `userId` (`userId`,`roleId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Дамп данных таблицы `acl`
+--
+
+INSERT INTO `acl` (`userId`, `roleId`) VALUES
+(4, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -18,13 +45,20 @@ CREATE TABLE IF NOT EXISTS `acl` (
 --
 
 CREATE TABLE IF NOT EXISTS `roles` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `alias` varchar(50) collate utf8_unicode_ci NOT NULL,
-  `fullName` varchar(200) collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`),
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `alias` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `fullName` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `alias` (`alias`),
   KEY `fullName` (`fullName`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Дамп данных таблицы `roles`
+--
+
+INSERT INTO `roles` (`id`, `alias`, `fullName`) VALUES
+(3, 'admin', 'SiteAdmin');
 
 -- --------------------------------------------------------
 
@@ -33,15 +67,22 @@ CREATE TABLE IF NOT EXISTS `roles` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `login` varchar(50) collate utf8_unicode_ci NOT NULL,
-  `password` varchar(200) collate utf8_unicode_ci NOT NULL,
-  `fullName` varchar(200) collate utf8_unicode_ci default NULL,
-  `email` varchar(200) collate utf8_unicode_ci NOT NULL,
-  `active` tinyint(3) unsigned NOT NULL default '0',
-  `code` varchar(50) collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`),
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `fullName` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `active` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `code` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `fullName` (`fullName`),
   KEY `login` (`login`),
   KEY `code` (`code`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `fullName`, `email`, `active`, `code`) VALUES
+(4, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'SiteAdmin', 'admin@localhost', 1, '');
